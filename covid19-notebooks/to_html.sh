dc4f21e758b6e7901694ddf935e0c78c8d818928
Pauline Ribeyre <ribeyre@uchicago.edu>
2020-04-15 10:10:19 -0500

    script to convert notebooks to html (#22)

---
+++ covid19-notebooks/to_html.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+mkdir -p $DIR/html-notebooks
+
+function convert_to_html {
+    echo Converting to HTML: $1
+    jupyter nbconvert --to html $DIR/$1 --output-dir $DIR/html-notebooks
+}
+
+convert_to_html "chicago-seir-forecast/covid19_seir.ipynb"
+convert_to_html "jhu-summary-overview/COVID-19-JHU_data_analysis_04072020.ipynb"
+convert_to_html "kaggle-demographics/kaggle_data_analysis_04072020.ipynb"
