b1940fd22009e1178030f5a1c83ac34c5a72e109
Andrew Prokhorenkov <aprokh@uchicago.edu>
2020-04-20 09:42:47 -0500

    fix: match project name in data commons

--- ds4c_kaggle_dataset/main.py
+++ ds4c_kaggle_dataset/main.py
@@ -4,7 +4,6 @@ from metadata_helper import MetadataHelper
 
 if __name__ == "__main__":
     base_url = "https://chicagoland.pandemicresponsecommons.org"
-    base_url = "https://qa-covid19.planx-pla.net"
     program_name = "open"
     project_code = "DS4C"
     metadata_helper = MetadataHelper(base_url, program_name=program_name, project_code=project_code, access_token=token)
@@ -98,32 +97,3 @@ if __name__ == "__main__":
             # break
 
         metadata_helper.batch_submit_records()
-
-# from importlib import import_module
-#
-# if __name__ == "__main__":
-#     base_url = "http://revproxy-service"
-#     base_url = "https://chicagoland.pandemicresponsecommons.org"
-#     # base_url = "https://qa-covid19.planx-pla.net"
-#     token = os.environ.get("ACCESS_TOKEN")
-#     if not token:
-#         raise Exception(
-#             "Need ACCESS_TOKEN environment variable (token for user with read and write access)"
-#         )
-#
-#     job_name = os.environ.get("JOB_NAME")
-#     job_name = "IDPH_ZIPCODE"
-#     if not job_name:
-#         raise Exception(
-#             "Need JOB_NAME environment variable (specification on which ETL job to run)"
-#         )
-#
-#     job_module = job_name.lower()
-#     job_class = job_name.upper()
-#
-#     etl_module = import_module(f"etl.{job_module}")
-#     etl = getattr(etl_module, job_class)
-#
-#     job = etl(base_url, token)
-#     job.files_to_submissions()
-#     job.submit_metadata()
