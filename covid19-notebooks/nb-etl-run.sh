7919c7ed24ebe3d9808092e9637f73f27deb8094
Pauline <ribeyre@uchicago.edu>
2020-06-18 10:30:37 -0500

    move nb-etl dockerfile

--- covid19-notebooks/nb-etl-run.sh
+++ covid19-notebooks/nb-etl-run.sh
@@ -12,7 +12,7 @@ echo "Running notebook $FILE..."
 jupyter nbconvert --to notebook --inplace --execute "$FILE"
 
 echo "Running top10 script..."
-python generate_plots.py
+python generate_top10_plots.py
 
 echo "Copying to S3 bucket..."
 if [[ -n "$S3_BUCKET" ]]; then
