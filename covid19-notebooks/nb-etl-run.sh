8447e0e96ebc8177310021b862c92a10f10588db
mattgarvin1 <mattgarvin1@gmail.com>
2020-06-25 11:24:23 -0500

    test more cores nb-etl job

--- covid19-notebooks/nb-etl-run.sh
+++ covid19-notebooks/nb-etl-run.sh
@@ -27,7 +27,11 @@ echo "Running bayes-by-county..."
 cd /nb-etl/bayes-by-county/
 
 # sh run.sh <stan_model> <deaths_cutoff> <nIterations>
-sh run.sh us_base 10 8000
+# ACTUAL RUN
+# sh run.sh us_base 10 8000
+# DEBUG RUN
+sh run.sh us_base 400 1000
+
 
 # copy images to S3 under prefix "bayes-by-county"
 # directory structure:
