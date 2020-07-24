9bc24a42406729a2c6e05b7081017e4e7d332243
mattgarvin1 <mattgarvin1@gmail.com>
2020-07-24 14:47:43 -0500

    remove comment

--- covid19-notebooks/nb-etl-run.sh
+++ covid19-notebooks/nb-etl-run.sh
@@ -26,8 +26,6 @@ echo "Running bayes-by-county..."
 cd /nb-etl/bayes-by-county/
 
 # sh run.sh <stan_model> <deaths_cutoff> <nIterations>
-
-# actual
 sh run.sh us_mobility 10 200
 
 # copy images to S3 under prefix "bayes-by-county"
