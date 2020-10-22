fb68915db10d851ae0ffed00e297705029ce5d13
Giang Bui <giangbui0816@gmail.com>
2020-10-22 09:33:11 -0500

    chore(code): refactor

--- covid19-etl/etl/ncbi_file.py
+++ covid19-etl/etl/ncbi_file.py
@@ -23,12 +23,13 @@ DATA_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 class NCBI_FILE(base.BaseETL):
+    """Class for handle NCBI data file"""
+
     def __init__(self, base_url, access_token, s3_bucket):
         super().__init__(base_url, access_token, s3_bucket)
 
         self.program_name = "open"
         self.project_code = "NCBI"
-        self.queue = asyncio.Queue()
         self.access_number_set = set()
 
         self.file_helper = AsyncFileHelper(
@@ -56,96 +57,9 @@ class NCBI_FILE(base.BaseETL):
             "virus_sequence_notc": ["hmmsearch_notc/hmmsearch_notc.json"],
         }
 
-    def get_existed_accession_numbers(self, node_name):
-        query_string = "{ " + node_name + " (first:0) { submitter_id } }"
-        response = self.metadata_helper.query_node_data(query_string)
-        if response.status_code != 200:
-            raise Exception(
-                f"ERROR: can not get a list of accession numbers of the node {node_name}"
-            )
-        records = response.json()["data"][node_name]
-        return records
-
-    async def file_to_submissions(self, filepath):
-        filename = os.path.basename(filepath)
-        did, rev, md5, size = await self.file_helper.async_find_by_name(filename)
-        if not did:
-            guid = await self.file_helper.async_upload_file(filepath)
-            print(f"file {filepath.name} uploaded with guid: {guid}")
-        else:
-            await self.file_helper.async_update_authz(did, rev)
-            print(f"file {filepath.name} exists in indexd... skipping...")
-        os.remove(filepath)
-
-    async def process_row(
-        self, line, node_name, ext, headers, accession_number, n_rows, f, excluded_set
-    ):
-        r1 = re.findall("[SDE]RR\d+", line)
-        if len(r1) == 0 and n_rows == 0:
-            return f, accession_number
-        assert (
-            len(r1) == 1
-        ), "The files have changed (expected {} contains accession number in the format of [SDE]RR\d+). We may need to update the ETL code".format(
-            line
-        )
-        read_accession_number = r1[0]
-
-        if read_accession_number in excluded_set:
-            return f, accession_number
-        # self.queue.put_nowait(read_accession_number)
-        self.access_number_set.add(f"{node_name}_{read_accession_number}")
-
-        if not accession_number or int(read_accession_number[3:]) != int(
-            accession_number[3:]
-        ):
-            if f:
-                f.close()
-                # asyncio.ensure_future(self.file_to_submissions(
-                #     Path(f"{DATA_PATH}/{node_name}_{accession_number}.{ext}"))
-                # )
-                await self.file_to_submissions(
-                    Path(f"{DATA_PATH}/{node_name}_{accession_number}.{ext}")
-                )
-
-            accession_number = read_accession_number
-            f = open(f"{DATA_PATH}/{node_name}_{accession_number}.{ext}", "w")
-            if headers:
-                f.write(headers)
-        f.write(line)
-        return f, accession_number
-
-    async def index_ncbi_data_file(
-        self, node_name, ext, key, excluded_set, headers=None
-    ):
-        s3 = boto3.resource("s3", config=Config(signature_version=UNSIGNED))
-        s3_object = s3.Object(self.bucket, key)
-        line_stream = codecs.getreader("utf-8")
-        accession_number = None
-        n_rows = 0
-        f = None
-        try:
-            for line in line_stream(s3_object.get()["Body"]):
-                f, accession_number = await self.process_row(
-                    line,
-                    node_name,
-                    ext,
-                    headers,
-                    accession_number,
-                    n_rows,
-                    f,
-                    excluded_set,
-                )
-                n_rows += 1
-                if n_rows % 1000 == 0:
-                    print(f"Finish process {n_rows} of file {node_name}")
-        except Exception as e:
-            # close the file
-            if f:
-                f.close()
-            await asyncio.sleep(10)
-        self.queue.put_nowait(None)
-
     def submit_metadata(self):
+        """Main function to submit the data"""
+
         start = time.strftime("%X")
         loop = asyncio.get_event_loop()
         tasks = []
@@ -167,6 +81,136 @@ class NCBI_FILE(base.BaseETL):
             loop.close()
         end = time.strftime("%X")
         print(f"Running time: From {start} to {end}")
+
+    def get_existed_accession_numbers(self, node_name):
+        """
+        Get a list of existed accession numbers
+
+        Args:
+            node_name(str): node name
+        Returns:
+            list(str): list of accession numbers
+        """
+
+        query_string = "{ " + node_name + " (first:0) { submitter_id } }"
+        response = self.metadata_helper.query_node_data(query_string)
+        if response.status_code != 200:
+            raise Exception(
+                f"ERROR: can not get a list of accession numbers of the node {node_name}"
+            )
+        records = response.json()["data"][node_name]
+        return records
+
+    async def index_ncbi_data_file(
+        self, node_name, ext, key, excluded_set, headers=None
+    ):
+        """
+        Asynchornous function to index NCBI data file
+
+        Args:
+            node_name(str): node name
+            ext(str): the file extension (json|tsv|csv)
+            key(str): the s3 object key where the file lives
+            excluded_set(set): a set of accession number need to be ignored
+            headers(str): headers of the input file
+        """
+
+        s3 = boto3.resource("s3", config=Config(signature_version=UNSIGNED))
+        s3_object = s3.Object(self.bucket, key)
+        line_stream = codecs.getreader("utf-8")
+        accession_number = None
+        n_rows = 0
+        f = None
+        for line in line_stream(s3_object.get()["Body"]):
+            try:
+                f, accession_number = await self.parse_row(
+                    line,
+                    node_name,
+                    ext,
+                    headers,
+                    accession_number,
+                    n_rows,
+                    f,
+                    excluded_set,
+                )
+                n_rows += 1
+                if n_rows % 10000 == 0:
+                    print(f"Finish process {n_rows} of file {node_name}")
+            except Exception as e:
+                print(f"ERROR: {e}")
+                # close the file
+                if f:
+                    f.close()
+                await asyncio.sleep(10)
+        self.queue.put_nowait(None)
+
+    async def parse_row(
+        self, line, node_name, ext, headers, accession_number, n_rows, f, excluded_set
+    ):
+        """
+        Parse a data row
+
+        Args:
+            line(str): a data row
+            node_name(str): node name
+            ext(str): the file extension (json|tsv|csv)
+            headers(str): headers of the input file
+            accession_number(str): the current accession number
+            n_rows(int): number of rows processed
+            f(file): the opening file
+            excluded_set(set): a set of accession number need to be ignored
+
+        Returns:
+            f(file): the opening file
+            accession_number(string): the current accession number
+        """
+
+        r1 = re.findall("[SDE]RR\d+", line)
+        if len(r1) == 0 and n_rows == 0:
+            return f, accession_number
+        assert (
+            len(r1) == 1
+        ), "The files have changed (expected {} contains accession number in the format of [SDE]RR\d+). We may need to update the ETL code".format(
+            line
+        )
+        read_accession_number = r1[0]
+
+        if read_accession_number in excluded_set:
+            return f, accession_number
+        self.access_number_set.add(f"{node_name}_{read_accession_number}")
+
+        if not accession_number or read_accession_number != accession_number:
+            if f:
+                f.close()
+                await self.file_to_submissions(
+                    Path(f"{DATA_PATH}/{node_name}_{accession_number}.{ext}")
+                )
+
+            accession_number = read_accession_number
+            f = open(f"{DATA_PATH}/{node_name}_{accession_number}.{ext}", "w")
+            if headers:
+                f.write(headers)
+        f.write(line)
+        return f, accession_number
+
+    async def file_to_submissions(self, filepath):
+        """Asynchornous call to submit the data file"""
+
+        filename = os.path.basename(filepath)
+        did, rev, md5, size, authz = await self.file_helper.async_find_by_name(filename)
+        if not did:
+            try:
+                guid = await self.file_helper.async_upload_file(filepath)
+                print(f"file {filepath.name} uploaded with guid: {guid}")
+            except Exception as e:
+                print(f"ERROR: Fail to upload file {filepath}. Detail {e}")
+        elif not authz:
+            try:
+                await self.file_helper.async_update_authz(did, rev)
+                print(f"file {filepath.name} exists in indexd... skipping...")
+            except Exception as e:
+                print(f"ERROR: Fail to update authz for file {did}. Detail {e}")
+        os.remove(filepath)
 
 
 class SRA_TAXONOMY_FILE(NCBI_FILE):
--- covid19-etl/helper/async_file_helper.py
+++ covid19-etl/helper/async_file_helper.py
@@ -3,17 +3,9 @@ from aiohttp import ClientSession
 import requests
 
 
-async def async_upload_file(path, url):
-    with open(path, "rb") as data:
-        try:
-            async with ClientSession() as session:
-                async with session.put(url, data=data) as r:
-                    return r.status
-        except Exception as err:
-            print(err)
-
-
 class AsyncFileHelper:
+    """ Asynchronous file helper class"""
+
     def __init__(self, base_url, program_name, project_code, access_token):
         self.base_url = base_url
         self.program_name = program_name
@@ -21,6 +13,8 @@ class AsyncFileHelper:
         self.headers = {"Authorization": f"Bearer {access_token}"}
 
     async def async_find_by_name(self, filename):
+        """Asynchronous call to fine the indexd record given a filename"""
+
         url = f"{self.base_url}/index/index?file_name={filename}"
         async with ClientSession() as session:
             async with session.get(url) as r:
@@ -34,10 +28,13 @@ class AsyncFileHelper:
                     rev = data["records"][0]["rev"]
                     md5sum = data["records"][0]["hashes"]["md5"]
                     size = data["records"][0]["size"]
-                    return did, rev, md5sum, size
-                return None, None, None, None
+                    authz = data["records"][0]["authz"]
+                    return did, rev, md5sum, size, authz
+                return None, None, None, None, None
 
     async def async_update_authz(self, did, rev):
+        """Asynchronous update authz field for did"""
+
         url = f"{self.base_url}/index/index/{did}?rev={rev}"
         async with ClientSession() as session:
             async with session.put(
@@ -52,6 +49,8 @@ class AsyncFileHelper:
                 r.raise_for_status()
 
     async def async_get_presigned_url(self, filename):
+        """Asynchronous call to get presigned url"""
+
         upload_url = f"{self.base_url}/user/data/upload"
         body_json = {"file_name": filename}
         async with ClientSession() as session:
@@ -63,9 +62,18 @@ class AsyncFileHelper:
                 return data["url"], data["guid"]
 
     async def async_upload_file(self, path):
+        """Asynchronous call to upload the file"""
+
+        async def _async_upload_file(path, url):
+            with open(path, "rb") as data:
+                async with ClientSession() as session:
+                    async with session.put(url, data=data) as r:
+                        return r.status
+            return None
+
         basename = path.name
         presigned_url, guid = await self.async_get_presigned_url(basename)
-        upload_status = await async_upload_file(path, presigned_url)
+        upload_status = await _async_upload_file(path, presigned_url)
         if upload_status == requests.codes.ok:
             return guid
         return None
