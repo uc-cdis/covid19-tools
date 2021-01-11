cb8784481a0be36d0fb4c2a2a4df18e3ad17c4e1
mattgarvin1 <mattgarvin1@gmail.com>
2021-01-11 13:12:57 -0600

    test small sim

--- covid19-notebooks/nb-etl-run.sh
+++ covid19-notebooks/nb-etl-run.sh
@@ -26,7 +26,10 @@ echo "Running bayes-by-county..."
 cd /nb-etl/bayes-by-county/
 
 # sh run.sh <stan_model> <deaths_cutoff> <nIterations>
-sh run.sh us_mobility 10 200
+# sh run.sh us_mobility 10 200
+
+# TEST #
+sh run.sh us_mobility 1100 150
 
 # copy images to S3 under prefix "bayes-by-county"
 # directory structure:
