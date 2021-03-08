21b5a09b1dc69bd6a1bc8f923f4ad9a124182b92
Jawad Qureshi <qureshi@uchicago.edu>
2021-03-08 15:36:38 -0600

    Add ability to post results of job to slack

---
+++ covid19-notebooks/run-with-slack.sh
@@ -0,0 +1,16 @@
+if ! bash "/nb-etl/nb-etl-run.sh"; then
+    echo "JOBFAIL: nb-etl-job"
+    if [[ -n "$slackWebHook" && "$doSlack" == "true" ]]; then
+        echo "Posting failed message to slack.."
+        payload="{\"attachments\": [{\"fallback\": \"JOBFAIL: nb-etl job on ${gen3Env}\",\"color\": \"#ff0000\",\"pretext\": \"JOBFAIL: nb-etl job on ${gen3Env}\",\"author_name\": \"Pod name: ${HOSTNAME}\",\"title\": \"NB-ETL JOB FAILED\",\"text\": \"JOBFAIL: nb-etl job on ${gen3Env}\",\"ts\": "$(date +%s)"}]}"
+        echo "${payload}"
+        curl -X POST --data-urlencode "payload=${payload}" "${slackWebHook}"
+    fi
+else
+    echo "JOBSUCCESS: nb-etl-job"
+    if [[ -n "$slackWebHook" && "$doSlack" == "true" ]]; then
+        echo "Posting success message to slack.."
+        payload="{\"attachments\": [{\"fallback\": \"JOBSUCCESS: nb-etl job on ${gen3Env}\",\"color\": \"#2EB67D\",\"pretext\": \"JOBFAIL: nb-etl job on ${gen3Env}\",\"author_name\": \"Pod name: ${HOSTNAME}\",\"title\": \"NB-ETL JOB SUCCEDED :tada:\",\"text\": \"JOBSUCCESS: nb-etl job on ${gen3Env}\",\"ts\": \"$(date +%s)\"}]}"
+        curl -X POST --data-urlencode "payload=${payload}" "${slackWebHook}"
+    fi
+fi
