2617df7a42e2612225d9d8599a2f503457450f29
Pauline <ribeyre@uchicago.edu>
2021-03-30 18:33:39 -0500

    Fix run-with-slack success message

--- covid19-notebooks/covid19-notebook-etl-run-with-slack.sh
+++ covid19-notebooks/covid19-notebook-etl-run-with-slack.sh
@@ -10,7 +10,7 @@ else
     echo "JOBSUCCESS: covid19-notebook-etl-job"
     if [[ -n "$slackWebHook" && "$slackWebHook" != "None" ]]; then
         echo "Posting success message to slack.."
-        payload="{\"attachments\": [{\"fallback\": \"JOBSUCCESS: covid19-notebook-etl job on ${gen3Env}\",\"color\": \"#2EB67D\",\"pretext\": \"JOBFAIL: covid19-notebook-etl job on ${gen3Env}\",\"author_name\": \"Pod name: ${HOSTNAME}\",\"title\": \"COVID19-NOTEBOOK-ETL JOB SUCCEDED :tada:\",\"text\": \"JOBSUCCESS: covid19-notebook-etl job on ${gen3Env}\",\"ts\": \"$(date +%s)\"}]}"
+        payload="{\"attachments\": [{\"fallback\": \"JOBSUCCESS: covid19-notebook-etl job on ${gen3Env}\",\"color\": \"#2EB67D\",\"pretext\": \"JOBSUCCESS: covid19-notebook-etl job on ${gen3Env}\",\"author_name\": \"Pod name: ${HOSTNAME}\",\"title\": \"COVID19-NOTEBOOK-ETL JOB SUCCEDED :tada:\",\"text\": \"JOBSUCCESS: covid19-notebook-etl job on ${gen3Env}\",\"ts\": \"$(date +%s)\"}]}"
         curl -X POST --data-urlencode "payload=${payload}" "${slackWebHook}"
     fi
 fi
