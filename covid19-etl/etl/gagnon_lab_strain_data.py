2fb1755dea59d6d897f3d6183dc4a7b0181a1b21
John Dombrowski <john@westloop.io>
2021-04-20 16:51:54 -0500

    feat(siu-data-sources) adding google mobility data and gagnon strain data that both track data on the county level. these are to be used for the new covd map built by the folks at siu

---
+++ covid19-etl/etl/gagnon_lab_strain_data.py
@@ -0,0 +1,100 @@
+from datetime import datetime
+import os
+from contextlib import closing
+import csv
+import requests
+import boto3
+import json
+
+from etl import base
+from utils.metadata_helper import MetadataHelper
+
+def getDiffDaysSinceDataEpoch(newDate):
+    diff = newDate - datetime(2020, 1, 22)
+    return diff.days
+
+class GAGNON_LAB_STRAIN_DATA(base.BaseETL):
+    def __init__(self, base_url, access_token, s3_bucket):
+        self.base_url = base_url
+        self.access_token = access_token
+        self.s3_bucket = s3_bucket
+        self.nested_dict = {}
+        self.s3_client = boto3.client('s3')
+        self.url = 'https://raw.githubusercontent.com/gagnonlab/ncov-data/master/gagnon_data.csv'
+        self.expected_headers = ['countyFIPS','date','19A','19B','20A','20A.EU2','20B','20C','20D','20E (EU1)','20G','20H/501Y.V2','20I/501Y.V1','20J/501Y.V3']
+        self.column_headers = {
+            'countyFIPS': 0,
+            'date': 1,
+            '19A': 2,
+            '19B': 3,
+            '20A': 4,
+            '20A.EU2': 5,
+            '20B': 6,
+            '20C': 7,
+            '20D': 8,
+            '20E (EU1)': 9,
+            '20G': 10,
+            '20H/501Y.V2': 11,
+            '20I/501Y.V1': 12,
+            '20J/501Y.V3': 13
+        }
+
+    def files_to_submissions(self):
+        self.parse_mobility_file()
+
+    def submit_metadata(self):
+        self.publish_to_s3()
+
+    def parse_mobility_file(self):
+        # token = os.environ.get('GIT_AUTH_TOKEN')
+        # if token is None:
+        #     print("GIT_AUTH_TOKEN missing from env vars, No Strain data collected.")
+        #     return None
+        # headers = {'Authorization': 'token %s' % token}
+        print("Getting data from {}".format(self.url))
+        with closing(requests.get(self.url, stream=True)) as r:
+            f = (line.decode("utf-8") for line in r.iter_lines())
+            reader = csv.reader(f, delimiter=",", quotechar='"')
+            headers = next(reader)
+
+            assert (
+                headers == self.expected_headers
+            ), "CSV headers have changed (expected {}, got {}). We may need to update the ETL code".format(
+                self.expected_headers, headers
+            )
+
+            print('Headers were checked and now parsing data')
+
+            for row in reader:
+                self.parse_row(row)    
+
+    def parse_row(self, row):
+
+        if not row:  # ignore empty rows
+            return
+
+        if row[self.column_headers['countyFIPS']] not in self.nested_dict:
+            self.nested_dict[self.column_headers['countyFIPS']] = []
+        
+        self.nested_dict[self.column_headers['countyFIPS']].append({
+            'DE': getDiffDaysSinceDataEpoch(datetime.strptime(row[self.column_headers['date']],'%Y-%m-%d')),
+            '19A': row[self.column_headers['19A']],
+            '19B': row[self.column_headers['19B']],
+            '20A': row[self.column_headers['20A']],
+            '20A.EU2': row[self.column_headers['20A.EU2']],
+            '20B': row[self.column_headers['20B']],
+            '20C': row[self.column_headers['20C']],
+            '20D': row[self.column_headers['20D']],
+            '20E (EU1)': row[self.column_headers['20E (EU1)']],
+            '20G': row[self.column_headers['20G']],
+            '20H/501Y.V2': row[self.column_headers['20H/501Y.V2']],
+            '20I/501Y.V1': row[self.column_headers['20I/501Y.V1']],
+            '20J/501Y.V3': row[self.column_headers['20J/501Y.V3']]
+        })
+
+    def publish_to_s3(self):
+        print("Uploading mobility data json to S3...")
+        self.s3_client.put_object(Body=str(json.dumps(self.nested_dict)), Bucket=self.s3_bucket, Key='gagnon_lab_strain_data.json')
+        print("Done!")
+    
+
---
+++ covid19-etl/etl/google_mobility.py
@@ -0,0 +1,110 @@
+from datetime import datetime
+import os
+from contextlib import closing
+import csv
+import requests
+import boto3
+import json
+
+from etl import base
+from utils.metadata_helper import MetadataHelper
+
+"""
+getting data in the below format for the mobility json
+
+{
+    <county_code> : [
+        {
+        "date": date,
+        "days_elapsed": int,
+        "retail_and_recreation_percent_change_from_baseline": int,
+        "grocery_and_pharmacy_percent_change_from_baseline": int,
+        "parks_percent_change_from_baseline": int,
+        "transit_stations_percent_change_from_baseline": int,
+        "workplaces_percent_change_from_baseline": int,
+        "residential_percent_change_from_baseline": int
+        },
+        ...
+    ]
+}
+"""
+
+def getDiffDaysSinceDataEpoch(newDate):
+    diff = newDate - datetime(2020, 1, 22)
+    return diff.days
+
+class GOOGLE_MOBILITY(base.BaseETL):
+    def __init__(self, base_url, access_token, s3_bucket):
+        self.base_url = base_url
+        self.access_token = access_token
+        self.s3_bucket = s3_bucket
+        self.nested_dict = {}
+        self.s3_client = boto3.client('s3')
+        self.url = 'https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv'
+        self.expected_headers = ['country_region_code','country_region','sub_region_1','sub_region_2','metro_area','iso_3166_2_code','census_fips_code','place_id','date','retail_and_recreation_percent_change_from_baseline','grocery_and_pharmacy_percent_change_from_baseline','parks_percent_change_from_baseline','transit_stations_percent_change_from_baseline','workplaces_percent_change_from_baseline','residential_percent_change_from_baseline']
+        self.column_headers = {
+            'census_fips_code': 6,
+            'place_id': 7,
+            'date': 8,
+            'retail_and_recreation_percent_change_from_baseline': 9,
+            'grocery_and_pharmacy_percent_change_from_baseline': 10,
+            'parks_percent_change_from_baseline': 11,
+            'transit_stations_percent_change_from_baseline': 12,
+            'workplaces_percent_change_from_baseline': 13,
+            'residential_percent_change_from_baseline': 14
+        }
+
+    def files_to_submissions(self):
+        self.parse_mobility_file()
+
+    def submit_metadata(self):
+        self.publish_to_s3()
+
+    def parse_mobility_file(self):
+        print("Getting data from {}".format(self.url))
+        with closing(requests.get(self.url, stream=True)) as r:
+            f = (line.decode("utf-8") for line in r.iter_lines())
+            reader = csv.reader(f, delimiter=",", quotechar='"')
+            headers = next(reader)
+
+            assert (
+                headers == self.expected_headers
+            ), "CSV headers have changed (expected {}, got {}). We may need to update the ETL code".format(
+                self.expected_headers, headers
+            )
+
+            print('Headers were checked and now parsing data')
+
+            for row in reader:
+                self.parse_row(row)    
+
+    def parse_row(self, row):
+
+        if not row:  # ignore empty rows
+            return
+
+        if row[0] == 'US' and row[2] == 'Illinois':
+            return
+
+        if row[self.column_headers['census_fips_code']] not in self.nested_dict:
+            self.nested_dict[self.column_headers['census_fips_code']] = []
+        
+        self.nested_dict[self.column_headers['census_fips_code']].append({
+            'place_id': row[self.column_headers['place_id']],
+            'daysElapsed': getDiffDaysSinceDataEpoch(datetime.strptime( row[self.column_headers['date']],'%Y-%m-%d')),
+            'retail_and_recreation': row[self.column_headers['retail_and_recreation_percent_change_from_baseline']],
+            'grocery_and_pharmacy': row[self.column_headers['grocery_and_pharmacy_percent_change_from_baseline']],
+            'parks': row[self.column_headers['parks_percent_change_from_baseline']],
+            'transit_stations': row[self.column_headers['transit_stations_percent_change_from_baseline']],
+            'workplaces': row[self.column_headers['workplaces_percent_change_from_baseline']],
+            'residential': row[self.column_headers['residential_percent_change_from_baseline']]
+        })
+
+        self.json_file = json.dumps(self.nested_dict) 
+
+    def publish_to_s3(self):
+        print("Uploading mobility data json to S3...")
+        self.s3_client.put_object(Body=str(json.dumps(self.nested_dict)), Bucket=self.s3_bucket, Key='google_mobility_data.json')
+        print("Done!")
+    
+
