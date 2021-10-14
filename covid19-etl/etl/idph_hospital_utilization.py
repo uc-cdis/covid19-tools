11903582671cb79fee47023fdd2ecc1be93d6a45
Sai Shanmukha <nss10@outlook.com>
2021-10-14 12:16:42 -0500

    printing summary clinicals to see the duplications

--- covid19-etl/etl/idph_hospital_utilization.py
+++ covid19-etl/etl/idph_hospital_utilization.py
@@ -104,7 +104,8 @@ class IDPH_HOSPITAL_UTILIZATION(base.BaseETL):
                     summary_clinical = self.parse_historical(
                         summary_location_submitter_id, utilization
                     )
-
+                    print(summary_clinical)
+                    print("-" * 10)
                     self.summary_clinicals.append(summary_clinical)
 
     def parse_historical(self, summary_location_submitter_id, utilization):
@@ -158,6 +159,5 @@ class IDPH_HOSPITAL_UTILIZATION(base.BaseETL):
         for sc in self.summary_clinicals:
             sc_record = {"type": "summary_clinical"}
             sc_record.update(sc)
-            print(sc)
             self.metadata_helper.add_record_to_submit(sc_record)
         self.metadata_helper.batch_submit_records()
