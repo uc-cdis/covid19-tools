c909c56f413ea4c7f549ea262e7de1bf8d1384cb
Sai Shanmukha <nss10@outlook.com>
2021-11-02 16:58:47 -0500

    Fix typo in shell script

--- generative_bayes_model/gbm-run.sh
+++ generative_bayes_model/gbm-run.sh
@@ -4,7 +4,7 @@ echo "Running pymc3_generative_model..."
 python3 pymc3_generative_model.py;
 
 echo "Copying results to S3 bucket..."
-if [[ -n "$S3_BUCKET" ]]; thenm
+if [[ -n "$S3_BUCKET" ]]; then
     aws s3 cp "*.svg" "$S3_BUCKET/generative_bayes_model/"
 else
     echo "Copying to s3 failed due to S3_BUCKET env_var not being set"
