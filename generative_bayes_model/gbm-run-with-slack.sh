6446cf77f647a50a006ba0e91fbf2f52b781db8f
Sai Shanmukha Narumanchi <nss10@outlook.com>
2021-11-03 12:49:11 -0500

    Replace gym with Generative Bayes Model
    Co-authored-by: Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>

--- generative_bayes_model/gbm-run-with-slack.sh
+++ generative_bayes_model/gbm-run-with-slack.sh
@@ -1,16 +1,16 @@
 if ! bash "$HOME/gbm/gbm-run.sh"; then
-    echo "JOB FAILED: gbm-run.sh"
+    echo "JOB FAILED: generative Bayes model"
     if [[ -n "$slackWebHook" && "$slackWebHook" != "None" ]]; then
         echo "Posting failed message to slack.."
-        payload="{\"attachments\": [{\"fallback\": \"JOB FAILED: gbm-run.sh job on ${gen3Env}\",\"color\": \"#ff0000\",\"title\": \"JOB FAILED: gbm-run.sh job on ${gen3Env}\",\"text\": \"Pod name: ${HOSTNAME}\",\"ts\": "$(date +%s)"}]}"
+        payload="{\"attachments\": [{\"fallback\": \"JOB FAILED: generative Bayes model job on ${gen3Env}\",\"color\": \"#ff0000\",\"title\": \"JOB FAILED: generative Bayes model job on ${gen3Env}\",\"text\": \"Pod name: ${HOSTNAME}\",\"ts\": "$(date +%s)"}]}"
         echo "${payload}"
         curl -X POST --data-urlencode "payload=${payload}" "${slackWebHook}"
     fi
 else
-    echo "JOB SUCCESS: gbm-run.sh"
+    echo "JOB SUCCESS: generative Bayes model"
     if [[ -n "$slackWebHook" && "$slackWebHook" != "None" ]]; then
         echo "Posting success message to slack.."
-        payload="{\"attachments\": [{\"fallback\": \"JOB SUCCESS: gbm-run.sh job on ${gen3Env}\",\"color\": \"#2EB67D\",\"title\": \"JOB SUCCESS: gbm-run.sh job on ${gen3Env}\",\"text\": \"Pod name: ${HOSTNAME}\",\"ts\": \"$(date +%s)\"}]}"
+        payload="{\"attachments\": [{\"fallback\": \"JOB SUCCESS: generative Bayes model job on ${gen3Env}\",\"color\": \"#2EB67D\",\"title\": \"JOB SUCCESS: generative Bayes model job on ${gen3Env}\",\"text\": \"Pod name: ${HOSTNAME}\",\"ts\": \"$(date +%s)\"}]}"
         curl -X POST --data-urlencode "payload=${payload}" "${slackWebHook}"
     fi
 fi
