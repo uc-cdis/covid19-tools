a3fb1e5d7614c49ce5c074c0ddfacb543974994e
Sai Shanmukha <nss10@outlook.com>
2021-11-03 08:44:57 -0500

    Updated aws s3 sync command

--- generative_bayes_model/gbm-run.sh
+++ generative_bayes_model/gbm-run.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+if [[ -z "${S3_BUCKET-}" ]]; then
+    echo "No S3 bucket provided (use env var S3_BUCKET)"
+    exit 1
+fi
+
 echo "Running pymc3_generative_model..."
 python3 pymc3_generative_model.py;
 
 echo "Copying results to S3 bucket..."
 if [[ -n "$S3_BUCKET" ]]; then
-    aws s3 cp "*.svg" "$S3_BUCKET/generative_bayes_model/"
-else
-    echo "Copying to s3 failed due to S3_BUCKET env_var not being set"
+    aws s3 sync "." "$S3_BUCKET/generative_bayes_model/" "--exclude" "*" "--include" "*.svg"
 fi
