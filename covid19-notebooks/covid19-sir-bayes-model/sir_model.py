10f08829baca5a64bd3c607fc93e757d765e3049
wangfan860 <fan1@uchicago.edu>
2022-02-22 10:37:00 -0600

    improve docker size

--- covid19-notebooks/covid19-sir-bayes-model/sir_model.py
+++ covid19-notebooks/covid19-sir-bayes-model/sir_model.py
@@ -31,7 +31,7 @@ date_data_begin = confirmed_cases.loc[
     (confirmed_cases["Province_State"] == "Illinois")
     & (confirmed_cases["Admin2"] == "Cook"),
     :,
-].columns[-70]
+].columns[-72]
 month, day, year = map(int, date_data_end.split("/"))
 
 data_begin = date_data_begin
