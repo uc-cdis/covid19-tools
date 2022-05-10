9d51aa3b5502ed2d3f7b71f5a65880b425ab3e70
urvi-occ <urvi@occ-data.org>
2022-05-10 16:29:14 -0500

    test case added

--- covid19-etl/tests/test_cityofchicago.py
+++ covid19-etl/tests/test_cityofchicago.py
@@ -1,9 +1,14 @@
 import os
-
+import csv
 from etl.cityofchicago import CITYOFCHICAGO
+from utils.format_helper import (
+    derived_submitter_id,
+    format_submitter_id,
+)
 
-
-INPUT_DATA_PATH = os.path.join(os.path.dirname(__file__), "test_cityofchicago_input.csv")  # TODO use test data that makes sense, for example with edge cases
+INPUT_DATA_PATH = os.path.join(
+    os.path.dirname(__file__), "test_data/test_cityofchicago_input.csv"
+)
 
 
 def get_test_etl():
@@ -22,25 +27,51 @@ def get_test_etl():
         return MockResponse()
 
     class MockMetadataHelper:
-        # TODO the functions can return whatever makes sense for your tests,
-        # you can pass values to `get_test_etl` depending on your test case
         def get_existing_summary_locations(self):
             return []
-    
+
         def get_latest_submitted_date_idph(self):
             return None
+
+        def get_last_submission(self):
+            # The following returns date for the first entry in dataset, which is `2020-03-01`
+            return "2020-03-01"
 
     etl = CITYOFCHICAGO("base_url", "access_token", "s3_bucket")
     etl.get = lambda *args, **kwargs: mock_get(args)
     etl.metadata_helper = MockMetadataHelper()
+    etl.summary_location_submitter_id = format_submitter_id(
+        "summary_location",
+        {"country": "US", "state": "IL", "city": "chicago"},
+    )
     return etl
 
 
-def test_no_existing_data():
+def test_cityofchicago():
     # run the ETL
     etl = get_test_etl()
-    etl.files_to_submissions()
+    etl.get_summary_location(etl.summary_location_submitter_id)
+    with open(INPUT_DATA_PATH, "r") as file:
+        file_data = csv.reader(file)
+        headers = next(file_data)
+        for row in file_data:
+            etl.parse_row(headers, row, etl.summary_location_submitter_id)
 
-    assert etl.summary_locations == "TODO"
-    assert etl.summary_clinicals == "TODO"
-    assert etl.summary_group_demographics == "TODO"
+    assert etl.summary_locations == {
+        "summary_location_us_il_chicago": {
+            "country_region": "US",
+            "county": "Cook",
+            "province_state": "IL",
+            "projects": [{"code": "cityofchicago"}],
+        }
+    }
+    assert (
+        etl.summary_clinicals["summary_clinical_us_il_chicago_2022-04-22"]["count"]
+        == 683
+    )  # Matching cases_total count for date `2022-04-22` from test_data
+    assert (
+        len(etl.summary_group_demographics) == 180
+    )  # 10 rows in original dataset and each row can make 18 different summary_group_demographics rows
+    assert (
+        etl.last_submission_identifier == "2022-04-24"
+    )  # according to dataset, used for testing, it doesnt have hopitalization data after `2022-04-24`
